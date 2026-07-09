ab2e7ff0aed69ad478633a733c52e09657e0a7a3
VM-221b <vedamall221b@gmail.com>
2026-07-09 16:21:08 +0530

    Update TaskController.java

--- src/main/java/com/demo/taskboard/controller/TaskController.java
+++ src/main/java/com/demo/taskboard/controller/TaskController.java
@@ -15,32 +15,27 @@ public class TaskController {
     @Autowired
     private TaskService service;
 
-    // Create a new task
     @PostMapping
     public Task createTask(@RequestBody Task task) {
         return service.createTask(task);
     }
 
-    // Get all tasks
     @GetMapping
     public List<Task> getAllTasks() {
         return service.getAllTasks();
     }
 
-    // Get task by ID
     @GetMapping("/{id}")
     public Optional<Task> getTaskById(@PathVariable Long id) {
         return service.getTaskById(id);
     }
 
-    // Update an existing task
     @PutMapping("/{id}")
     public Task updateTask(@PathVariable Long id, @RequestBody Task task) {
         task.setId(id);
         return service.updateTask(task);
     }
 
-    // Delete task by ID
     @DeleteMapping("/{id}")
     public void deleteTask(@PathVariable Long id) {
         service.deleteTask(id);
